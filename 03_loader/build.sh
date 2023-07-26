df0559755a6853a65ed7e38a55c843c121b3ccf0
ASUKA39 <573470041@qq.com>
2023-07-27 00:17:41 +0800

    add tips about gcc4.4

--- 03_loader/build.sh
+++ 03_loader/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-rm -rf ./build/*
+# rm -rf ./build/*
 rm ~/bochs/hd60M.img
 
 cd build
@@ -14,8 +14,8 @@ dd if=./build/mbr of=./build/hd60M.img bs=512 count=1 conv=notrunc
 nasm -o ./build/loader ./boot/loader.S -I ./boot/include/
 dd if=./build/loader of=./build/hd60M.img bs=512 count=4 conv=notrunc seek=2
 
-gcc -o ./build/main.o -c -m32 ./kernel/main.c
-ld -m elf_i386 -Ttext 0x00001500 -e main -o ./build/kernel.bin  ./build/main.o
+# gcc -o ./build/main.o -c -m32 ./kernel/main.c
+# ld -m elf_i386 -Ttext 0x00001500 -e main -o ./build/kernel.bin  ./build/main.o
 dd if=./build/kernel.bin of=./build/hd60M.img bs=512 count=200 conv=notrunc seek=9
 
 cp ./build/hd60M.img ~/bochs/
